2d6cec5a4d18d4dcf779775e0732d68f644994f7
Colin Skinner <colinaskinner@gmail.com>
2025-06-23 07:56:14 -0700

    rk4 first commit

---
+++ src/math/integrators/rk4.h
@@ -0,0 +1,18 @@
+#ifndef RK4_H
+#define RK4_H
+
+#include <Eigen/Dense>
+
+#include "math/utilities.h"
+
+namespace Cesium::Math {
+
+inline Eigen::VectorXd rk4(double dt, Eigen::VectorXd y) {
+    // std::cout << "YAY"
+}
+
+} // namespace Cesium::Math
+
+
+
+#endif
