defd90080508d2bcd294e55062d9a0067603dac5
Colin Skinner <colinaskinner@gmail.com>
2025-06-23 09:22:11 -0700

    Changed interface of derivative function to be more unit-testable

--- src/simulation/physics/dynamics/EquationsOfMotion.cpp
+++ src/simulation/physics/dynamics/EquationsOfMotion.cpp
@@ -8,22 +8,46 @@ namespace Cesium::Sim::Physics {
 
 StateDerivative RigidBody::derivative(Vector3d force, Vector3d torque)
 {
+
+    this->force = force;
+    this->torque = torque;
+
+    StateDerivative state_dot = StateDerivative::from_vec(
+        compute_derivative(this->state.to_vec())
+    );
+    
+    return state_dot;
+}
+
+VectorXd RigidBody::compute_derivative(const VectorXd &state) const
+{
+    Vector3d r = state.segment<3>(0);
+    Vector3d v = state.segment<3>(3);
+    Quaterniond q(state(6), state(7), state(8), state(9));
+    Vector3d w = state.segment<3>(10);
+
+    VectorXd dot(13);
+
     StateDerivative state_dot;
 
     // Position derivative is velocity 
-    state_dot.drdt = this->state.v;
+    dot.segment<3>(0) = v;
 
     // Velocity derivative is acceleration (Schaub 2.15)
-    state_dot.dvdt = force / this->mass;
+    dot.segment<3>(3) = this->force / this->mass;
 
-    // Quaternion derivative is based on hamiltonian prodict (Schaub 3.109)
-    state_dot.dqdt = Cesium::Math::hamilton_product(this->state.q, this->state.w);
+    // Quaternion derivative is based on hamilton prodict (Schaub 3.109)
+    Quaterniond product = Cesium::Math::hamilton_product(this->state.q, this->state.w);
+    dot(6) = product.w();
+    dot(7) = product.x();
+    dot(8) = product.y();
+    dot(9) = product.z();
 
     // Angular velocity derivative based on (Schaub 4.34)
     // dwdt = I.inv * (τ - w x (Iw) )
-    state_dot.dwdt = this->InertiaTensor.inverse() * (torque - this->state.w.cross(this->InertiaTensor * this->state.w));
+    dot.segment<3>(10) = this->InertiaTensor.inverse() * (this->torque - this->state.w.cross(this->InertiaTensor * this->state.w));
 
-    return state_dot;
+    return dot;
 }
 
 } // namespace Cesium::Sim::Physics
--- src/simulation/physics/dynamics/EquationsOfMotion.h
+++ src/simulation/physics/dynamics/EquationsOfMotion.h
@@ -6,23 +6,43 @@
 
 namespace Cesium::Sim::Physics {
     
+//////////////////////////////////////////////////
+//                    State                     //
+//////////////////////////////////////////////////
+
 struct State {
     Eigen::Vector3d r{};
     Eigen::Vector3d v{};
     Eigen::Quaterniond q{};
     Eigen::Vector3d w{};
+
+    Eigen::VectorXd to_vec() const {
+        Eigen::VectorXd rslt(13);
+        rslt << r, v, q.w(), q.x(), q.y(), q.z(), w;
+        return rslt;
+    }
 };
+
+//////////////////////////////////////////////////
+//               StateDerivative                //
+//////////////////////////////////////////////////
 
 struct StateDerivative {
     Eigen::Vector3d drdt{};
     Eigen::Vector3d dvdt{};
     Eigen::Quaterniond dqdt{};
     Eigen::Vector3d dwdt{};
-};
 
-//////////////////////////////////////////////////
-//                Motion Helpers                //
-//////////////////////////////////////////////////
+    static StateDerivative from_vec(Eigen::VectorXd dot) {
+        StateDerivative rslt;
+        rslt.drdt = dot.segment<3>(0);
+        rslt.dvdt = dot.segment<3>(3);
+        rslt.dqdt = Eigen::Quaterniond(dot(6), dot(7), dot(8), dot(9));
+        rslt.dwdt = dot.segment<3>(10);
+
+        return rslt;
+    }
+};
 
 //////////////////////////////////////////////////
 //                  RigidBody                   //
@@ -33,31 +53,15 @@ struct RigidBody {
     double mass = 0.0;
     Eigen::Matrix3d InertiaTensor = Eigen::Matrix3d::Zero(3,3);
 
+    Eigen::Vector3d force{};
+    Eigen::Vector3d torque{};
+    
     double epoch_s;
 
     StateDerivative derivative(Eigen::Vector3d force, Eigen::Vector3d torque);
+
+    Eigen::VectorXd compute_derivative(const Eigen::VectorXd& state) const;
 };
-
-
-
-
-// struct RigidBody {
-//     double mass;
-//     Eigen::Matrix3d I{};
-// };
-
-// struct Disturbances {
-//     Eigen::Vector3d force;
-//     Eigen::Vector3d torque;
-// };
-
-
-// StateDerivative compute_state_derivative(RigidBody force, Disturbances disturbances);
-
-
-
-
-
 
 } // namespace Cesium::Sim::Physics
 
